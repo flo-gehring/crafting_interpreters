3fb669534fbf0b427bf3620eec93b4d4e1c9955a
Flo Gehring <florian.gehring@pm.me>
2025-07-06 22:22:20 +0200

    Second Chapter

---
+++ jlox/src/main/java/de/friendlyhedgehog/tool/GenerateAst.java
@@ -0,0 +1,85 @@
+package de.friendlyhedgehog.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", List.of(
+                "Binary     : Expr left, Token operator, Expr right",
+                "Grouping   : Expr expression",
+                "Literal    : Object value",
+                "Unary      : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(
+            String outputDir,
+            String baseName,
+            List<String> types
+    ) throws IOException {
+        String path = outputDir + "\\" + baseName + ".java";
+        try (PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8)) {
+            writer.println("package de.friendlyhedgehog.lox;");
+            writer.println();
+            writer.println("import java.util.List;");
+            writer.println();
+            writer.println("abstract class " + baseName + " {");
+            defineVisitor(writer, baseName, types);
+            for (String type : types) {
+                String[] split = type.split(":");
+                String className = split[0].trim();
+                String fields = split[1].trim();
+                defineType(writer, baseName, className, fields);
+            }
+            writer.println();
+            writer.println("    abstract <R> R accept(Visitor<R> visitor); ");
+            writer.println("}");
+        }
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> { ");
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println("    }");
+    }
+
+    private static void defineType(
+            PrintWriter writer,
+            String baseName,
+            String className,
+            String fieldList
+    ) throws IOException {
+        writer.println("    static class " + className + " extends " + baseName + " {");
+        writer.println("        " + className + "(" + fieldList + "){");
+        String[] fields = fieldList.split(",");
+        for (String field : fields) {
+            String[] split = field.trim().split(" ");
+            String name = split[1];
+            writer.println("            this." + name + " = " + name + ";");
+        }
+        writer.println("        }");
+
+        // Visitor Pattern
+        writer.println();
+        writer.println("        @Override ");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
+        for (String field : fields) {
+            writer.println("        final " + field + ";");
+        }
+        writer.println("    }");
+    }
+}
