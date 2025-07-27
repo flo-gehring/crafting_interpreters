f2400d0a43581fda175707988aa12be097739d2b
Flo Gehring <florian.gehring@pm.me>
2025-07-27 12:33:25 +0200

    Intro Bytecode

--- clox/main.c
+++ clox/main.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
 
 int main(int argc, char *argv[]) {
-
+    // -((1.2 + 3.4) / 5.6)
     initVM();
-
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2);
@@ -30,5 +31,86 @@ int main(int argc, char *argv[]) {
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
+
+
+    // 1 * 2 + 3
+    printf("1 * 2 + 3 \n");
+    printf("----------\n");
+    initVM();
+    initChunk(&chunk);
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_MULTIPLY, 123);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
+    disassembleChunk(&chunk, "test 1 * 2 + 3");
+    interpret(&chunk);
+    freeVM();
+    freeChunk(&chunk);
+
+    // 1 + 2 * 3
+    printf("1 + 2 * 3 \n");
+    printf("----------\n");
+    initVM();
+    initChunk(&chunk);
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_MULTIPLY, 123);
+
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
+    disassembleChunk(&chunk, "test 1 + 2 * 3");
+    interpret(&chunk);
+    freeVM();
+    freeChunk(&chunk);
+
+    // 3 - 2 - 1
+    printf("3 - 2 - 1 \n");
+    printf("----------\n");
+    initVM();
+    initChunk(&chunk);
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_SUBTRACT, 123);
+
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_SUBTRACT, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
+    disassembleChunk(&chunk, "test 3 - 2 - 1");
+    interpret(&chunk);
+    freeVM();
+    freeChunk(&chunk);
+
+
     return 0;
 }
