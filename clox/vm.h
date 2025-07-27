fc055d4784f119b4a47fe786dea0a31ab9013251
Flo Gehring <florian.gehring@pm.me>
2025-07-27 21:46:45 +0200

    Scanning

--- clox/vm.h
+++ clox/vm.h
@@ -4,7 +4,7 @@
 #define clox_vm_h
 
 #include "chunk.h"
-
+#include "compiler.h"
 #define STACK_MAX 256
 
 typedef struct {
@@ -22,7 +22,7 @@ typedef enum {
 
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk * chunk);
+InterpretResult interpret(const char* source);
 void push(Value value);
 Value pop();
 
